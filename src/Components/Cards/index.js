a159196804fdfa8d861e1b65bcba8b2467896c57
Burunga <monte_marcos@hotmail.com>
2024-03-06 21:04:02 -0300

    Upload de encarde da Loja Biju da Ju

---
+++ src/Components/Cards/index.js
@@ -0,0 +1,50 @@
+import style from '@/styles/Card.module.css';
+
+import Image from 'next/image';
+import Profile from '../../../public/assets/belussi.jpg';
+import Check from '../../../public/assets/check.svg';
+
+export default function Card() {
+    return (
+        <div className={style.container}>
+
+            <div className={style.title_box}>
+                <Image className={style.title_picture} src={Profile} alt="Foto da Proprietária Juliana Belussi" />
+            </div>
+
+            <div className={style.description_box}>
+                <h2 className={style.description_title}>
+                    Atendimento Presencial
+                </h2>
+
+                <p>Experimente a praticidade de escolher suas semijoias presencialmente! Agende uma visita ou um café e tenha acesso exclusivo a uma seleção personalizada de peças incríveis, feitas especialmente para você.</p>
+
+                <ul className={style.list}>
+
+                    <li>
+                        <Image className={style.check} src={Check} alt="Check" />
+                        <span>Comodidade: </span>
+                        Ter acesso a todas as peças do estoque.
+                    </li>
+                    <li>
+                        <Image className={style.check} src={Check} alt="Check" />
+                        <span>Personalização: </span>
+                        Recomendações adaptadas ao seu estilo.
+                    </li>
+                    <li>
+                        <Image className={style.check} src={Check} alt="Check" />
+                        <span>Experimente antes de comprar: </span>
+                        Veja as peças de perto.
+                    </li>
+                </ul>
+
+
+                <a className={style.button} href="https://web.whatsapp.com/send?phone=5513991303306">
+                    agende um horário
+                </a>
+
+            </div>
+
+        </div>
+    )
+}
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
+      </Head>
       <body>
         <Main />
         <NextScript />
